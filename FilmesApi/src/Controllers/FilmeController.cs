166e911689a7e88175ed2af99f51be43a4032245
Diego rocha <diegolprocha@gmail.com>
2023-12-05 13:40:42 -0300

    Adicionado arquivos a pasta src

--- FilmesApi/src/Controllers/FilmeController.cs
+++ FilmesApi/src/Controllers/FilmeController.cs
@@ -1,21 +1,21 @@
-﻿using FilmesApi.Models;
+﻿using FilmesApi.src.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace FilmesApi.Controllers;
+namespace FilmesApi.src.Controllers;
 
 [ApiController]
-[Route ("[controller]")]
+[Route("[controller]")]
 public class FilmeController : ControllerBase
 {
     private static List<Filme> filmes = new List<Filme>();
 
     [HttpPost]
     public void AdicionaFilme([FromBody] Filme filme)
-    { 
+    {
         filmes.Add(filme);
         Console.WriteLine(filme.Titulo);
         Console.WriteLine(filme.Duracao);
 
-    }       
-        
+    }
+
 }
